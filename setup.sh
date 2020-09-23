1f97b8c1cf093ea75c58e8ef1fe7e161e097347f
anonymous <64162091+anonymous770@users.noreply.github.com>
2020-09-23 06:48:46 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+pkg install python
+pip install --upgrade pip
+pip install pytube3
+mv yt.py .yt
+cp -r yt ~/../usr/bin
+chmod +x .yt
+cp -r .yt ~
+echo "\n\t\033[1;33mType \033[1;31myt\033[0m \033[1;33mfor start tool.\033[0m\n"
